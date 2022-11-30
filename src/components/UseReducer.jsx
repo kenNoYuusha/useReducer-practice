482b83577b7ff57369ea25105a55c25c30f1b024
Ren Amaki <jorge_luis1709@hotmail.com>
2022-11-29 20:55:24 -0600

    use Reducer declarativo

--- src/components/UseReducer.jsx
+++ src/components/UseReducer.jsx
@@ -1,20 +1,39 @@
 import React, { useReducer, useEffect } from "react";
 
-const CODIGO = "paradigma";
-
+const CODIGO_SECRETO = "paradigma";
 
 const UseReducer = ({ name }) => {
   const [state, dispatch] = useReducer(reducerObject, initialState);
   const { value, error, loading, deleted, confirm } = state;
+ 
+  //ACCIONES
+  const onConfirm = () => {
+    dispatch({ type: actionType.CONFIRM });
+  }
+  const onError = () => {
+    dispatch({ type: actionType.ERROR });
+  }
+  const onWrite = ({target : {value}}) => {
+    dispatch({ type: actionType.WRITE, payload: value })
+  }
+  const onCheck = () => {
+    dispatch({ type: actionType.CHECK })
+  }
+  const onReset = () => {
+    dispatch({ type: actionType.RESET })
+  }
+  const onDelete = () => {
+    dispatch({ type: actionType.DELETE })
+  }
 
   //EFECTOS
   useEffect(() => {
     if (!!loading) {
       setTimeout(() => {
-        if (value === CODIGO) {
-          dispatch({ type: "CONFIRM" });
+        if (value === CODIGO_SECRETO) {
+          onConfirm();
         } else {
-          dispatch({ type: "ERROR" });
+          onError();
         }
       }, 3000);
     }
@@ -35,11 +54,11 @@ const UseReducer = ({ name }) => {
           className="px-4 py-2 rounded-md text-black outline-none"
           placeholder="Codigo de seguridad"
           value={value}
-          onChange={(e) => dispatch({ type: "WRITE", payload: e.target.value })}
+          onChange={onWrite}
         />
         <button
           className="ml-4 px-4 py-2 bg-indigo-700 rounded-md hover:bg-indigo-800 active:bg-indigo-900"
-          onClick={() => dispatch({ type: "CHECK" })}
+          onClick={onCheck}
         >
           Comprobar
         </button>
@@ -53,13 +72,13 @@ const UseReducer = ({ name }) => {
         <div className="flex flex-row gap-x-4">
           <button
             className="ml-4 px-4 py-2 bg-zinc-800 rounded-md hover:bg-zinc-900 active:bg-black border-2 border-white"
-            onClick={() => dispatch({ type: "RESET" })}
+            onClick={onReset}
           >
             Cancel
           </button>
           <button
             className="ml-4 px-4 py-2 bg-indigo-700 rounded-md hover:bg-indigo-800 active:bg-indigo-900"
-            onClick={() => dispatch({ type: "DELETE" })}
+            onClick={onDelete}
           >
             Aceptar
           </button>
@@ -73,7 +92,7 @@ const UseReducer = ({ name }) => {
 
         <button
           className="px-4 py-2 bg-emerald-700 rounded-md hover:bg-emerald-800 active:bg-emerald-900"
-          onClick={() => dispatch({ type: "RESET" })}
+          onClick={onReset}
         >
           Undo
         </button>
@@ -90,119 +109,119 @@ const initialState = {
     confirm: false,
   };
 
-const reducerIf = (state, action) => {
-  if (action.type === "ERROR") {
-    return {
-      ...state,
-      error: true,
-      loading: false,
-      value: "",
-    };
-  } else if (action.type === "CONFIRM") {
-    return {
-      ...state,
-      error: false,
-      loading: false,
-      value: "",
-      confirm: true,
-    };
-  } else if (action.type === "CHECK") {
-    return {
-      ...state,
-      loading: true,
-    };
-  } else if (action.type === "DELETE") {
-    return {
-      ...state,
-      deleted: true,
-    };
-  } else if (action.type === "RESET") {
-    return {
-      ...state,
-      confirm: false,
-      deleted: false,
-    };
-  } else if (action.type === "WRITE") {
-    return {
-      ...state,
-      value: action.payload,
-    };
-  } else {
-    return { ...state };
-  }
-};
+// const reducerIf = (state, action) => {
+//   if (action.type === "ERROR") {
+//     return {
+//       ...state,
+//       error: true,
+//       loading: false,
+//       value: "",
+//     };
+//   } else if (action.type === "CONFIRM") {
+//     return {
+//       ...state,
+//       error: false,
+//       loading: false,
+//       value: "",
+//       confirm: true,
+//     };
+//   } else if (action.type === "CHECK") {
+//     return {
+//       ...state,
+//       loading: true,
+//     };
+//   } else if (action.type === "DELETE") {
+//     return {
+//       ...state,
+//       deleted: true,
+//     };
+//   } else if (action.type === "RESET") {
+//     return {
+//       ...state,
+//       confirm: false,
+//       deleted: false,
+//     };
+//   } else if (action.type === "WRITE") {
+//     return {
+//       ...state,
+//       value: action.payload,
+//     };
+//   } else {
+//     return { ...state };
+//   }
+// };
 
-const recuerSwitch = (state, action) => {
-  switch (action.type) {
-    case "ERROR":
-      return {
-        ...state,
-        error: true,
-        loading: false,
-        value: "",
-      };
-    case "CONFIRM":
-      return {
-        ...state,
-        error: false,
-        loading: false,
-        value: "",
-        confirm: true,
-      };
-    case "CHECK":
-      return {
-        ...state,
-        loading: true,
-      };
-    case "DELETE":
-      return {
-        ...state,
-        deleted: true,
-      };
-    case "RESET":
-      return {
-        ...state,
-        confirm: false,
-        deleted: false,
-      };
-    case "WRITE":
-      return {
-        ...state,
-        value: action.payload,
-      };
-    default:
-      return { ...state };
-  }
-};
+// const reducerSwitch = (state, action) => {
+//   switch (action.type) {
+//     case "ERROR":
+//       return {
+//         ...state,
+//         error: true,
+//         loading: false,
+//         value: "",
+//       };
+//     case "CONFIRM":
+//       return {
+//         ...state,
+//         error: false,
+//         loading: false,
+//         value: "",
+//         confirm: true,
+//       };
+//     case "CHECK":
+//       return {
+//         ...state,
+//         loading: true,
+//       };
+//     case "DELETE":
+//       return {
+//         ...state,
+//         deleted: true,
+//       };
+//     case "RESET":
+//       return {
+//         ...state,
+//         confirm: false,
+//         deleted: false,
+//       };
+//     case "WRITE":
+//       return {
+//         ...state,
+//         value: action.payload,
+//     };
+//     default:
+//         return { ...state };
+//     }
+// };
 
 const allActions = (state, payload) => ({
-  ERROR: {
+  [actionType.ERROR]: {
     ...state,
     error: true,
     loading: false,
     value: "",
   },
-  CONFIRM: {
+  [actionType.CONFIRM]: {
     ...state,
     error: false,
     loading: false,
     value: "",
     confirm: true,
   },
-  CHECK: {
+  [actionType.CHECK]: {
     ...state,
     loading: true,
   },
-  DELETE: {
+  [actionType.DELETE]: {
     ...state,
     deleted: true,
   },
-  RESET: {
+  [actionType.RESET]: {
     ...state,
     confirm: false,
     deleted: false,
   },
-  WRITE: {
+  [actionType.WRITE]: {
     ...state,
     value: payload,
   },
@@ -215,5 +234,14 @@ const reducerObject = (state, action) => {
     return { ...state };
   }
 };
+
+const actionType = {
+    CONFIRM : "CONFIRM",
+    ERROR : "ERROR",
+    CHECK : "CHECK",
+    DELETE : "DELETE",
+    RESET : "RESET",
+    WRITE : "WRITE",
+}
 
 export { UseReducer };
